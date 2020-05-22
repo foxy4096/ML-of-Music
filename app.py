7282d5718b51f8a1868bffc8e5f09fb9292fe9c7
Aditya Priyadarshi <54215788+adityapriyadarshi669@users.noreply.github.com>
2020-05-22 12:30:53 +0530

    Correct the spelling

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from sklearn.tree import DecisionTreeClassifier
 import pandas as pd
 clf = DecisionTreeClassifier()
 data = pd.read_csv("music.csv")
-X = data.drop(coloumn="genre")
+X = data.drop(columns="genre")
 y = data["genre"]
 clf.fit(X, y)
 predictions = clf.predict([[60, 1]])
