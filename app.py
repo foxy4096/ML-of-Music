b05ca087f82780c190b598f9d597b0c6d8f59b2e
Aditya Priyadarshi <54215788+adityapriyadarshi669@users.noreply.github.com>
2020-05-22 12:26:50 +0530

    add the code

---
+++ app.py
@@ -0,0 +1,9 @@
+from sklearn.tree import DecisionTreeClassifier
+import pandas as pd
+clf = DecisionTreeClassifier()
+data = pd.read_csv("music.csv")
+X = data.drop(coloumn="genre")
+y = data["genre"]
+clf.fit(X, y)
+predictions = clf.predict([[60, 1]])
+print(predictions)
